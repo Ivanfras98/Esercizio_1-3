d07cea39e980840a6b0c831212a4fb503d72ce2c
Riccardo Pirrello <riccardo.pirrello@gmail.com>
2022-07-19 12:45:11 +0200

    final

--- app.js
+++ app.js
@@ -12,11 +12,26 @@ async function Animal() {
     } else {
       nocturnal.push(finalAnimal);
     }
+    let img= document.createElement('img');
+    img.src = finalAnimal.image_link;
+   img.style.height = "500px";
+   img.style.width = "500px";
+   img.dataset.time = finalAnimal.active_time === `Diurnal` ? "Diurnal" : "Nocturnal";
+   document.querySelector("div").appendChild(img);
+
   }
   console.log(diurnal);
   console.log(nocturnal);
 }
 
-Animal();
+let select = document.querySelector("select");
+select.addEventListener("change", () => {
+document.querySelectorAll("[data-time]").forEach(item => item.style.display= "inline-block")
+if(select.value === "Nocturnal"){
+document.querySelectorAll("[data-time=Diurnal]").forEach(item => item.style.display= "none")
+} else if(select.value === "Diurnal"){
+  document.querySelectorAll("[data-time=Nocturnal]").forEach(item => item.style.display= "none")
+}
+})
 
-
+Animal()
