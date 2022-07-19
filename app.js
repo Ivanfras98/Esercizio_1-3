d5fcac52ceb7c959bb9ba351538c14248ab15235
Flavia Soreca <flaviasoreca@gmail.com>
2022-07-19 12:16:00 +0200

    second

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const API = "https://zoo-animal-api.herokuapp.com/animals/rand";
 const container = [];
+const diurnal = [];
+const nocturnal = [];
 async function Animal() {
   for (let i = 0; i < 20; i++) {
     let animal = await fetch(API);
     let finalAnimal = await animal.json();
     container.push(finalAnimal)
-    console.log(container) 
+    if (finalAnimal.active_time === `Diurnal`){
+      diurnal.push(finalAnimal);
+    } else {
+      nocturnal.push(finalAnimal);
+    }
   }
+  console.log(diurnal);
+  console.log(nocturnal);
 }
 
 Animal();
+
 
